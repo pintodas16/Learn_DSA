2128079f89840b9163063c53b8df153558896323
pinto das <pintodas.lu@gmail.com>
2022-06-03 03:28:50 +0600

    check_palindrome_using_recursion

--- recursion/palindrome_check.py
+++ recursion/palindrome_check.py
@@ -1,3 +1,13 @@
+# usign recusive_call to check palindrome or not 
+
+def r_is_palindrome(s,l,r):
+    if l>=r : return True
+    if s[l]!=s[r]:
+        return False
+    return  r_is_palindrome(s,l+1,r-1)
+      
+    
+
 # using two pointer 
 
 def is_palindrome(s,l,r):
@@ -13,3 +23,4 @@ s = input('Enter a string : ')
 length = len(s)
 ans = is_palindrome(s,0,length-1)
 print(ans)
+print(r_is_palindrome(s,0,length-1))
